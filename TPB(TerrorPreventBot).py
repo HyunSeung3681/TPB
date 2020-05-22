326614a2680cfab8f13b2dc67406e9a96e7bbb9b
HyunSeung3681 <63762950+HyunSeung3681@users.noreply.github.com>
2020-05-22 14:15:41 +0900

    Update TPB(TerrorPreventBot).py

--- TPB(TerrorPreventBot).py
+++ TPB(TerrorPreventBot).py
@@ -1,4 +1,4 @@
-﻿import asyncio
+import asyncio
 import discord
 import os
 
@@ -17,16 +17,11 @@ async def on_ready():
 async def on_message(message) :
     if message.author.bot:
         return None
-    if "ㅁ" in message.content and "ㅊ" in message.content:
+    if "ㅁㅊ" in message.content:
     	await message.delete()
     	await message.channel.send(message.author.mention + " 착한말^^")
     	channel = discord.utils.get(message.guild.text_channels, name="tpb-로그")
     	await channel.send(f"{message.author.mention}님이 욕설을 사용하셨습니다.\n사용 욕설 : ㅁㅊ\n메시지 내용 : {message.content}")
-    if "ㅅ" in message.content and "ㅂ" in message.content:
-    	await message.delete()
-    	await message.channel.send(message.author.mention + " 착한말^^")
-    	channel = discord.utils.get(message.guild.text_channels, name="tpb-로그")
-    	await channel.send(f"{message.author.mention}님이 욕설을 사용하셨습니다.\n사용 욕설 : ㅅㅂ\n메시지 내용 : {message.content}")
-        
+
 access_token=os.environ["BOT_TOKEN"]
 app.run(access_token)
