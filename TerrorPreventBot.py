2512d1a8d3a6d41f977400ddb0f194411ff1502b
HyunSeung3681 <63762950+HyunSeung3681@users.noreply.github.com>
2020-05-25 13:50:34 +0900

    Update TerrorPreventBot.py

--- TerrorPreventBot.py
+++ TerrorPreventBot.py
@@ -10,7 +10,7 @@ async def on_ready():
     print(app.user.name)
     print(app.user.id)
     print("=============")
-    messages = ["욕(채팅) 필터링 하는 중","t-도움을 입력해보세요!", f"{len(app.guilds)}개의 서버와 {len(app.users)}명의 유저와 함께"]
+    messages = ["욕(채팅) 필터링 하는 중","테러 도움을 입력해보세요!", f"{len(app.guilds)}개의 서버와 {len(app.users)}명의 유저와 함께"]
     while True:
         await app.change_presence(status=discord.Status.online, activity=discord.Activity(name=messages[0], type=discord.ActivityType.playing))
         messages.append(messages.pop(0))
@@ -30,9 +30,9 @@ async def on_message(message) :
     	await message.channel.send(f"{message.author.mention} 착한말^^")
     	channel = discord.utils.get(message.guild.text_channels, name="📃tpb-로그")
     	await channel.send(f"{message.author.mention}님이 욕설을 사용하셨습니다.\n사용 욕설 : ㅁㅊ\n메시지 내용 : {message.content}")
-    if message.content == "t-핑":
+    if message.content == "테러 핑":
         await message.channel.send(f":ping_pong: 퐁! 현재 핑은 {app.latency*1000}초입니다!")
-    if message.content == "t-도움":
+    if message.content == "테러 도움":
         await message.channel.send("아직 만들고 있답니다")
 
 access_token=os.environ["BOT_TOKEN"]
